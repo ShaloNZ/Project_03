94dfe54ac1ba79d62c15dd054709acba6ce854d7
Shalon Kodisingha <115321991+ShaloNZ@users.noreply.github.com>
2023-04-26 18:36:50 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -9,7 +9,7 @@ class Database
     {
 
         if (!isset(Database::$connection)) {
-            Database::$connection = new mysqli("localhost", "root", "Kodisingha@12", "viva_app_01", "3306");
+            Database::$connection = new mysqli("localhost", "root", "shalon@12", "viva_app_01", "3306");
         }
     }
 
